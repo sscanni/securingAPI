1d9a5ca11b358b188231c8951940f22487f9fb29
sscanni <steven.scanniello@snhu.edu>
2018-10-28 15:35:23 -0400

    Changes made for the "Password Protecting a Resource" Lesson

--- views.py
+++ views.py
@@ -4,24 +4,34 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-from flask import Flask
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
-engine = create_engine('sqlite:///users.db')
+engine = create_engine('sqlite:///users.db?check_same_thread=False')
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
+@auth.verify_password
+def verify_password(username, password):
+    user = session.query(User).filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
 @app.route('/api/users', methods = ['POST'])
 def new_user():
-    print ("got here")
     username = request.json.get('username')
     password = request.json.get('password')
     if username is None or password is None:
         abort(400) # missing arguments
     if session.query(User).filter_by(username = username).first() is not None:
-        abort(400) # existing user
+        print "existing user"
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
     user = User(username = username)
     user.hash_password(password)
     session.add(user)
@@ -35,6 +45,10 @@ def get_user(id):
         abort(400)
     return jsonify({'username': user.username})
 
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
 
 if __name__ == '__main__':
     app.debug = True
